8b20e61defee28f9b408b9cce7bcbff7f514420f
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-14 22:38:51 +0200

    Another solution with Futures

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -1,35 +1,48 @@
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 object CheckoutSystem {
   val Products: Map[String, BigDecimal] = Map("Apple" -> 0.60, "Orange" -> 0.25)
 
-  def costOfProducts(boughtProducts: List[String]): BigDecimal = {
-    val productsAfterOffers = orangeOffer(appleOffer(boughtProducts))
-    productsAfterOffers.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
-  }
+  def costOfProducts(boughtProducts: List[String]): Future[BigDecimal] =
+    Future(boughtProducts.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP))
 
-  def appleOffer(boughtProducts: List[String]): List[String] = {
-    val noOfApples = boughtProducts.count(_ == "Apple")
-    noOfApples % 2 match {
-      case 0 if(noOfApples > 0) => boughtProducts.filter(_ != "Apple") ++ List.fill(noOfApples/2)("Apple")
-      case 1 => boughtProducts.filter(_ != "Apple") ++ List.fill((noOfApples + 1)/2)("Apple")
-      case _ => boughtProducts
+  def appleOffer(noOfApples: Int): List[String] = {
+    if(noOfApples == 0) List()
+    else noOfApples % 2 match {
+      case 0 => List.fill(noOfApples / 2)("Apple")
+      case 1 => List.fill((noOfApples + 1) / 2)("Apple")
     }
   }
 
-  def orangeOffer(boughtProducts: List[String]): List[String] = {
-    val noOfOranges = boughtProducts.count(_ == "Orange")
-    if(noOfOranges < 3) boughtProducts
+  def orangeOffer(noOfOranges: Int): List[String] = {
+    if(noOfOranges < 3) List.fill(noOfOranges)("Orange")
     else noOfOranges % 3 match {
-      case 0 => boughtProducts.filter(_ != "Orange") ++ List.fill(noOfOranges * 2/3)("Orange")
-      case 1 => boughtProducts.filter(_ != "Orange") ++ List.fill((noOfOranges * 2 + 1)/3)("Orange")
-      case 2 => boughtProducts.filter(_ != "Orange") ++ List.fill((noOfOranges + 1) * 2 / 3)("Orange")
+      case 0 => List.fill(noOfOranges * 2 / 3)("Orange")
+      case 1 => List.fill((noOfOranges * 2 + 1) / 3)("Orange")
+      case 2 => List.fill((noOfOranges + 1) * 2 / 3)("Orange")
     }
   }
 
   def main(args: Array[String]): Unit = {
     val boughtProducts: List[String] = List("Apple", "Apple", "Orange", "Apple")
 
-    val result = "\u00a3" + costOfProducts(boughtProducts)
+    val noOfApples = boughtProducts.count(_ == "Apple")
+    val noOfOranges = boughtProducts.count(_ == "Orange")
 
-    println("[" + boughtProducts.mkString(", ") + "] => " + result)
+    val applesAfterOffer = Future(appleOffer(noOfApples))
+    val orangesAfterOffer = Future(orangeOffer(noOfOranges))
+
+    val result = for{
+      apples <- applesAfterOffer
+      oranges <- orangesAfterOffer
+      totalCost <- costOfProducts(apples ++ oranges)
+    } yield totalCost
+
+    result.onComplete{
+      case Success(cost) => println("[" + boughtProducts.mkString(", ") + "] => " + "\u00a3" + cost)
+      case Failure(e) => println("Something goes wrong " + e)
+    }
   }
 }
