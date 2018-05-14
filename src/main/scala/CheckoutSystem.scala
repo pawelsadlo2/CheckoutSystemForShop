a45195d781da5322854e78acc30a016bb8759274
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-14 22:20:01 +0200

    Step 1 - Shopping cart

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -1,5 +1,14 @@
 object CheckoutSystem {
+  val Products: Map[String, BigDecimal] = Map("Apple" -> 0.60, "Orange" -> 0.25)
+
+  def costOfProducts(boughtProducts: List[String]): BigDecimal =
+    boughtProducts.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
   def main(args: Array[String]): Unit = {
-    
+    val boughtProducts: List[String] = List("Apple", "Apple", "Orange", "Apple")
+
+    val result = "\u00a3" + costOfProducts(boughtProducts)
+
+    println("[" + boughtProducts.mkString(", ") + "] => " + result)
   }
 }
