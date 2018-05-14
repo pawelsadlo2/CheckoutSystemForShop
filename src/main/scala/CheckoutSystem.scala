4a5caafa5915a7830a9cf8c0507c59f03151803c
PatrykRudnicki <patrudnicki@gmail.com>
2018-05-14 22:21:45 +0200

    Created method for apple offer

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -4,6 +4,15 @@ object CheckoutSystem {
   def costOfProducts(boughtProducts: List[String]): BigDecimal =
     boughtProducts.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
 
+  def appleOffer(boughtProducts: List[String]): List[String] = {
+    val noOfApples = boughtProducts.count(_ == "Apple")
+    noOfApples % 2 match {
+      case 0 if(noOfApples > 0) => boughtProducts.filter(_ != "Apple") ++ List.fill(noOfApples/2)("Apple")
+      case 1 => boughtProducts.filter(_ != "Apple") ++ List.fill((noOfApples + 1)/2)("Apple")
+      case _ => boughtProducts
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val boughtProducts: List[String] = List("Apple", "Apple", "Orange", "Apple")
 
