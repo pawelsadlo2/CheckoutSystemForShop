8db6762c6ef71c581435fca60f21cda486845abe
pawelsadlo2 <40578365+pawelsadlo2@users.noreply.github.com>
2018-07-05 23:46:54 +0200

    kilka pytań
    Paniczu to była dla mnie najszybsza droga zeby zadac pytania xd jak bedziesz chcial to usuniesz to co napisałemxd

--- src/main/scala/CheckoutSystem.scala
+++ src/main/scala/CheckoutSystem.scala
@@ -1,14 +1,14 @@
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext.Implicits.global //za co to odpowiada?
 import scala.util.{Failure, Success}
 
 object CheckoutSystem {
   val Products: Map[String, BigDecimal] = Map("Apple" -> 0.60, "Orange" -> 0.25)
 
   def costOfProducts(boughtProducts: List[String]): Future[BigDecimal] =
-    Future(boughtProducts.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP))
+    Future(boughtProducts.map(Products(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP))//dlaczego Future? kiedy zostanie obliczona wartość? czy dopiero przy próbie wyświetlenia result, czy może wcześniej?
 
-  def appleOffer(noOfApples: Int): List[String] = {
+  def appleOffer(noOfApples: Int): List[String] = {//tutaj np nie ma future? czemu akurat tu nie ma a w innych funkcjach jest?
     val noOfApplesAfterPromo = if(noOfApples == 0) 0
     else noOfApples % 2 match {
       case 0 => noOfApples / 2
@@ -17,7 +17,7 @@ object CheckoutSystem {
     List.fill(noOfApplesAfterPromo)("Apple")
   }
 
-  def orangeOffer(noOfOranges: Int): List[String] = {
+  def orangeOffer(noOfOranges: Int): List[String] = {//też bez future
     val noOfOrangesAfterPromo = if(noOfOranges < 3) noOfOranges
     else noOfOranges % 3 match {
       case 0 => noOfOranges * 2 / 3
@@ -27,20 +27,20 @@ object CheckoutSystem {
     List.fill(noOfOrangesAfterPromo)("Orange")
   }
 
-  def costWithAppliedOffers(boughtProducts: List[String]): Future[BigDecimal] = {
+  def costWithAppliedOffers(boughtProducts: List[String]): Future[BigDecimal] = {//
     val noOfApples = boughtProducts.count(_ == "Apple")
     val noOfOranges = boughtProducts.count(_ == "Orange")
 
-    val applesAfterOffer = Future(appleOffer(noOfApples))
+    val applesAfterOffer = Future(appleOffer(noOfApples))// wywołujesz funkcje której deklaracja jest bez future, ale wywołanie oznaczasz jako future, czy była by różnica jeżeli funkcje zadeklarowałbyś z future ale wywołał ją bez? co by sie stało, jeżeli i tu i tu byłoby future :)?
     val orangesAfterOffer = Future(orangeOffer(noOfOranges))
 
     val result = for{
-      apples <- applesAfterOffer
+      apples <- applesAfterOffer //co oznacza "<-", przypisanie?
       oranges <- orangesAfterOffer
-      totalCost <- costOfProducts(apples ++ oranges)
+      totalCost <- costOfProducts(apples ++ oranges)// czemu dwa plusy?
     } yield totalCost
 
-    result
+    result//jak będziesz miał chwile to na skype sie umowimy zebys mi wytlumaczyl jeszcze to for comprahension
   }
 
   def printWithPoundSign(value: BigDecimal): String = "\u00a3" + value
